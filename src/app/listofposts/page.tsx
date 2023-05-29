81736a3acc8e7bb6bdaf06445ec8711293b85ba3
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-05-29 15:07:08 +0100

    no store

--- src/app/listofposts/page.tsx
+++ src/app/listofposts/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 const getPostsData  = async () => {
 const res = await fetch("https://jsonplaceholder.typicode.com/posts")
 return res.json();
@@ -7,14 +9,22 @@ const getUsersData = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/users")
     return res.json();
 }
+// this one not static
+const getDogData= async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random",{
+    cache:"no-store",
+    });
+    return res.json();
+}
 
 export default async function ListOfPosts () {
-    const [posts,users] = await Promise.all([getPostsData(),getUsersData()]);
+    const [posts,users,dog] = await Promise.all([getPostsData(),getUsersData(), getDogData()]);
   
     return(
     
     <>
     <div>
+        <Image src={dog.message}  alt="dog" width={300}height={300}/>
 
         {posts.map((post:any) =>{
             return <p key={post.id}>{post.title}</p>
